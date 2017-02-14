f065957d41a759da4d46e08f58ee0a2d9ac44f60
Paul <paulklemstine@yahoo.com>
2017-02-14 03:49:17 -0600

    added runner process

---
+++ src/com/klemstinegroup/wub2/test/JavaProcess.java
@@ -0,0 +1,127 @@
+package com.klemstinegroup.wub2.test;
+
+import oracle.jrockit.jfr.JFR;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by Paul on 2/14/2017.
+ */
+public final class JavaProcess {
+
+    public static void main(String[] args){
+        JFrame jframe=new JFrame("exit");
+        Button cancel=new Button("exit");
+        jframe.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        jframe.getContentPane().add(cancel);
+        jframe.pack();
+        jframe.setVisible(true);
+
+        while(true) {
+            try {
+                exec(BeautifulKMGSRandReduce.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private JavaProcess() {
+    }
+
+    public static int exec(Class klass) throws IOException,
+            InterruptedException {
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome +
+                File.separator + "bin" +
+                File.separator + "java";
+        String classpath = System.getProperty("java.class.path");
+        String className = klass.getCanonicalName();
+
+        ProcessBuilder builder = new ProcessBuilder(
+                javaBin, "-cp", classpath, className);
+
+        Process process = builder.start();
+       InputStream is=process.getInputStream();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (process.isAlive()){
+                    try {
+                        while(is.available()>0){
+                            System.out.print((char)is.read());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+        process.waitFor();
+        return process.exitValue();
+    }
+
+}
